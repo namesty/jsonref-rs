ed335e611355d566e6c1e12690791e0df047cc50
namesty <nestor09amesty@gmail.com>
2022-11-06 00:56:00 +0100

    (chore): hoisting definitions

--- src/lib.rs
+++ src/lib.rs
@@ -36,14 +36,16 @@
 //! **Note**:  If the JSONSchema has recursive `$ref` only the first recursion will happen.
 //! This is to stop an infinate loop.
 
+use serde_json::json;
 use serde_json::Value;
+use snafu::{ResultExt, Snafu};
 use std::collections::HashMap;
 use std::env;
 use std::fs;
+use std::io;
 use std::mem;
 use std::path::PathBuf;
 use url::Url;
-use snafu::{Snafu, ResultExt};
 
 #[derive(Debug, Snafu)]
 pub enum Error {
@@ -53,32 +55,107 @@ pub enum Error {
         source: std::io::Error,
     },
     #[snafu(display("Could not open schema from url {}: {}", url, source))]
-    SchemaFromUrl {
-        url: String,
-        source: ureq::Error,
-    },
+    SchemaFromUrl { url: String, source: ureq::Error },
     #[snafu(display("Parse error for url {}: {}", url, source))]
     UrlParseError {
         url: String,
         source: url::ParseError,
     },
     #[snafu(display("schema from {} not valid JSON: {}", url, source))]
-    SchemaNotJson {
-        url: String,
-        source: std::io::Error,
-    },
+    SchemaNotJson { url: String, source: std::io::Error },
     #[snafu(display("schema from {} not valid JSON: {}", url, source))]
     SchemaNotJsonSerde {
         url: String,
         source: serde_json::Error,
     },
     #[snafu(display("json pointer {} not found", pointer))]
-    JsonPointerNotFound {
-        pointer: String,
-    },
+    JsonPointerNotFound { pointer: String },
     #[snafu(display("{}", "Json Ref Error"))]
-    JSONRefError {
-        source: std::io::Error,
+    JSONRefError { source: std::io::Error },
+}
+
+/// Trait used to remove Json Value's element
+pub trait Remove {
+    /// Method use to remove element in Json Values
+    fn remove(&mut self, json_pointer: &str) -> io::Result<Option<Value>>;
+}
+
+impl Remove for serde_json::Value {
+    /// # Examples: Remove an element in a table
+    /// ```
+    /// use serde_json::Value;
+    /// use json_value_remove::Remove;
+    ///
+    /// let mut array1: Value = serde_json::from_str(r#"{"my_table":["a","b","c"]}"#).unwrap();
+    /// assert_eq!(Some(Value::String("a".to_string())), array1.remove("/my_table/0").unwrap());
+    /// assert_eq!(r#"{"my_table":["b","c"]}"#, array1.to_string());
+    /// ```
+    /// # Examples: Remove a field from an object
+    /// ```
+    /// use serde_json::Value;
+    /// use json_value_remove::Remove;
+    ///
+    /// let mut object1: Value = serde_json::from_str(r#"{"field1.0":{"field1.1":"value1.1","field1.2":"value1.2"},"field2.0":"value2.0"}"#).unwrap();
+    /// assert_eq!(Some(Value::String("value1.2".to_string())), object1.remove("/field1.0/field1.2").unwrap());
+    /// assert_eq!(r#"{"field1.0":{"field1.1":"value1.1"},"field2.0":"value2.0"}"#,object1.to_string());
+    /// ```
+    fn remove(&mut self, json_pointer: &str) -> io::Result<Option<Value>> {
+        let fields: Vec<&str> = json_pointer.split("/").skip(1).collect();
+
+        remove(self, fields)
+    }
+}
+
+fn remove(json_value: &mut Value, fields: Vec<&str>) -> io::Result<Option<Value>> {
+    if fields.is_empty() {
+        return Ok(None);
+    }
+
+    let mut fields = fields.clone();
+    let field = fields.remove(0);
+
+    if field.is_empty() {
+        return Ok(None);
+    }
+
+    match fields.is_empty() {
+        true => match json_value {
+            Value::Array(vec) => {
+                let index = match field.parse::<usize>() {
+                    Ok(index) => index,
+                    Err(e) => {
+                        return Err(io::Error::new(
+                            io::ErrorKind::InvalidInput,
+                            format!(
+                                "{}. Can't find the field '{}' in {}.",
+                                e,
+                                field,
+                                json_value.to_string()
+                            ),
+                        ))
+                    }
+                };
+                let len = vec.len();
+                if index >= len {
+                    return Err(io::Error::new(
+                        io::ErrorKind::InvalidInput,
+                        format!(
+                            "removal index (is {}) should be < len (is {}) from {}",
+                            index,
+                            len,
+                            json_value.to_string()
+                        ),
+                    ));
+                }
+                Ok(Some(vec.remove(index)))
+            }
+            Value::Object(map) => Ok(map.remove(field)),
+            _ => Ok(None),
+        },
+        false => match json_value.pointer_mut(format!("/{}", field).as_str()) {
+            Some(json_targeted) => remove(json_targeted, fields),
+            None => Ok(None),
+        },
     }
 }
 
@@ -108,10 +185,10 @@ impl JsonRef {
         };
     }
 
-    /// Set a key to store the data that the `$ref` replaced. 
+    /// Set a key to store the data that the `$ref` replaced.
     ///
     /// This example uses `__reference__` as the key.
-    /// 
+    ///
     /// ```
     /// # use jsonref::JsonRef;
     /// # let jsonref = JsonRef::new();
@@ -145,11 +222,18 @@ impl JsonRef {
     /// deref a serde_json value directly. Uses the current working directory for any relative
     /// refs.
     pub fn deref_value(&mut self, value: &mut Value) -> Result<()> {
-        let anon_file_url = format!("file://{}/anon.json", env::current_dir().context(JSONRefError {})?.to_string_lossy());
+        let anon_file_url = format!(
+            "file://{}/anon.json",
+            env::current_dir()
+                .context(JSONRefError {})?
+                .to_string_lossy()
+        );
         self.schema_cache
             .insert(anon_file_url.clone(), value.clone());
 
-        self.deref(value, anon_file_url, &vec![])?;
+        let mut definitions = json!({});
+
+        self.deref(value, anon_file_url, &vec![], &mut definitions)?;
         Ok(())
     }
 
@@ -168,10 +252,19 @@ impl JsonRef {
     /// # assert_eq!(input_url, file_expected)
     /// ```
     pub fn deref_url(&mut self, url: &str) -> Result<Value> {
-        let mut value: Value = ureq::get(url).call().context(SchemaFromUrl {url: url.to_owned()})?.into_json().context(SchemaNotJson {url: url.to_owned()})?;
+        let mut value: Value = ureq::get(url)
+            .call()
+            .context(SchemaFromUrl {
+                url: url.to_owned(),
+            })?
+            .into_json()
+            .context(SchemaNotJson {
+                url: url.to_owned(),
+            })?;
 
         self.schema_cache.insert(url.to_string(), value.clone());
-        self.deref(&mut value, url.to_string(), &vec![])?;
+        let mut definitions = json!({});
+        self.deref(&mut value, url.to_string(), &vec![], &mut definitions)?;
         Ok(value)
     }
 
@@ -193,14 +286,23 @@ impl JsonRef {
     /// # assert_eq!(file_example, file_expected)
     /// ```
     pub fn deref_file(&mut self, file_path: &str) -> Result<Value> {
-        let file = fs::File::open(file_path).context(SchemaFromFile {filename: file_path.to_owned()})?;
-        let mut value: Value = serde_json::from_reader(file).context(SchemaNotJsonSerde {url: file_path.to_owned()})?;
+        let file = fs::File::open(file_path).context(SchemaFromFile {
+            filename: file_path.to_owned(),
+        })?;
+        let mut value: Value = serde_json::from_reader(file).context(SchemaNotJsonSerde {
+            url: file_path.to_owned(),
+        })?;
         let path = PathBuf::from(file_path);
         let absolute_path = fs::canonicalize(path).context(JSONRefError {})?;
         let url = format!("file://{}", absolute_path.to_string_lossy());
 
         self.schema_cache.insert(url.clone(), value.clone());
-        self.deref(&mut value, url, &vec![])?;
+        let mut definitions = json!({});
+        self.deref(&mut value, url, &vec![], &mut definitions)?;
+
+        let val = value.as_object_mut().unwrap();
+        val.insert("definitions".to_string(), definitions);
+
         Ok(value)
     }
 
@@ -209,6 +311,7 @@ impl JsonRef {
         value: &mut Value,
         id: String,
         used_refs: &Vec<String>,
+        definitions: &mut Value,
     ) -> Result<()> {
         let mut new_id = id;
         if let Some(id_value) = value.get("$id") {
@@ -218,10 +321,26 @@ impl JsonRef {
         }
 
         if let Some(obj) = value.as_object_mut() {
+            if let Some(defs) = obj.get_mut("definitions") {
+                let mut defs_clone = defs.clone();
+                obj.remove("definitions").unwrap();
+
+                if let Some(def_obj) = defs_clone.as_object_mut() {
+                    let accumulated_defs = definitions.as_object_mut().unwrap();
+                    for (key, val) in def_obj.iter_mut() {
+                        accumulated_defs.insert(key.to_string(), val.clone());
+                    }
+                }
+            }
+
             if let Some(ref_value) = obj.remove("$ref") {
                 if let Some(ref_string) = ref_value.as_str() {
-                    let id_url = Url::parse(&new_id).context(UrlParseError {url: new_id.clone()})?;
-                    let ref_url = id_url.join(ref_string).context(UrlParseError {url: ref_string.to_owned()})?;
+                    let id_url = Url::parse(&new_id).context(UrlParseError {
+                        url: new_id.clone(),
+                    })?;
+                    let ref_url = id_url.join(ref_string).context(UrlParseError {
+                        url: ref_string.to_owned(),
+                    })?;
 
                     let mut ref_url_no_fragment = ref_url.clone();
                     ref_url_no_fragment.set_fragment(None);
@@ -232,11 +351,23 @@ impl JsonRef {
                         None => {
                             if ref_no_fragment.starts_with("http") {
                                 ureq::get(&ref_no_fragment)
-                                    .call().context(SchemaFromUrl {url: ref_no_fragment.clone()})?
-                                    .into_json().context(SchemaNotJson {url: ref_no_fragment.clone()})?
+                                    .call()
+                                    .context(SchemaFromUrl {
+                                        url: ref_no_fragment.clone(),
+                                    })?
+                                    .into_json()
+                                    .context(SchemaNotJson {
+                                        url: ref_no_fragment.clone(),
+                                    })?
                             } else if ref_no_fragment.starts_with("file") {
-                                let file = fs::File::open(ref_url_no_fragment.path()).context(SchemaFromFile {filename: ref_no_fragment.clone()})?;
-                                serde_json::from_reader(file).context(SchemaNotJsonSerde {url: ref_no_fragment.clone()} )?
+                                let file = fs::File::open(ref_url_no_fragment.path()).context(
+                                    SchemaFromFile {
+                                        filename: ref_no_fragment.clone(),
+                                    },
+                                )?;
+                                serde_json::from_reader(file).context(SchemaNotJsonSerde {
+                                    url: ref_no_fragment.clone(),
+                                })?
                             } else {
                                 panic!("need url to be a file or a http based url")
                             }
@@ -261,7 +392,7 @@ impl JsonRef {
                     let mut new_used_refs = used_refs.clone();
                     new_used_refs.push(ref_url_string);
 
-                    self.deref(&mut schema, ref_no_fragment, &new_used_refs)?;
+                    self.deref(&mut schema, ref_no_fragment, &new_used_refs, definitions)?;
                     let old_value = mem::replace(value, schema);
 
                     if let Some(reference_key) = &self.reference_key {
@@ -275,7 +406,7 @@ impl JsonRef {
 
         if let Some(obj) = value.as_object_mut() {
             for obj_value in obj.values_mut() {
-                self.deref(obj_value, new_id.clone(), used_refs)?
+                self.deref(obj_value, new_id.clone(), used_refs, definitions)?
             }
         }
         Ok(())
@@ -386,4 +517,19 @@ mod tests {
         assert_eq!(file_example, file_expected)
     }
 
+    #[test]
+    fn test_defs() {
+        let mut jsonref = JsonRef::new();
+        jsonref.set_reference_key("__reference__");
+        let file_example = jsonref
+            .deref_file("fixtures/definitions/base.json")
+            .unwrap();
+
+        let file = fs::File::open("fixtures/definitions/expected.json").unwrap();
+        let file_expected: Value = serde_json::from_reader(file).unwrap();
+
+        println!("{}", serde_json::to_string_pretty(&file_example).unwrap());
+
+        assert_eq!(file_example, file_expected)
+    }
 }
